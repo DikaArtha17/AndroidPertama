0e52cf08e930ff0e24da18f8b634cb1c119364c7
MSI GAMING <arthdika17@gmail.com>
2024-10-08 12:15:40 +0700

    add

--- app/src/main/java/com/example/androidkothapp/MainActivity.kt
+++ app/src/main/java/com/example/androidkothapp/MainActivity.kt
@@ -12,6 +12,8 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     private lateinit var tvCounter: TextView
     private lateinit var btnAddNumber: Button
+    private lateinit var btnResetNumber: Button
+    private  lateinit var btnSubtractNumber: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         tvCounter = findViewById(R.id.tvCounter)
         btnAddNumber = findViewById(R.id.btnAddNumber)
+        btnResetNumber = findViewById(R.id.btnResetNumber)
+        btnSubtractNumber = findViewById(R.id.btnSubtractNumber)
         tvCounter.text = "1"
 
     }
@@ -26,6 +30,14 @@ class MainActivity : AppCompatActivity() {
     fun addNumber(v: View) {
         val currVal = tvCounter.text.toString().toInt()
         val nextVal = currVal + 1
+        tvCounter.text = nextVal.toString()
+    }
+    fun resetNumber(v: View){
+        tvCounter.text ="0"
+    }
+    fun subtractnumber(v: View) {
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal - 1
         tvCounter.text = nextVal.toString()
     }
 }
